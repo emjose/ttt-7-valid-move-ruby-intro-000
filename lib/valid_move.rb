41e219b5ab2702368816ad75fda2951292bc4413
Emmanuel Jose <emjose@gmail.com>
2020-03-08 15:49:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,14 @@
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+  def position_taken?(array, index)
+    if array[index] == " " || array[index] == "" || array[index] == nil
       return false
     else
       return true
     end
   end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
+  def on_board?(number)
+    if number.between?(0, 8) == true
       return true
     else
       return false
